d054d0893ad9c3867c05b302154aed9e9e70eed1
Sergey <example@gmail.com>
2022-05-17 16:42:58 +0300

    added script to count films and set grades

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt("Один из последних просмотренных фильмов", "");
+let b = prompt("На сколько оцените его?", "");
+let c =prompt("Один из последних просмотренных фильмов", "");
+let d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+console.log(typeof(b));
